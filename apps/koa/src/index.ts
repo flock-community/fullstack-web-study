82ce042145f8147b4f55577f685c278e16df138c
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2023-11-17 12:18:59 +0100

    update index file
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- apps/koa/src/index.ts
+++ apps/koa/src/index.ts
@@ -1,16 +1,31 @@
-import Koa from "koa";
 import Router from "@koa/router";
+import { createPost, getPosts } from "js-app-db";
+import Koa from "koa";
+import type { KoaBodyMiddlewareOptions } from "koa-body";
+import { koaBody } from "koa-body";
 import View from "koa-views";
 
 const app = new Koa();
 const router = new Router();
-const render = View(`${process.cwd()}/src/views`, { extension: 'pug'});
+const render = View(`${process.cwd()}/src/views`, { extension: "pug" });
 
-const root = async (ctx: any) => {
-  await ctx.render('index')
+const parserOpts: Partial<KoaBodyMiddlewareOptions> = {
+  multipart: true,
+  urlencoded: true,
 };
 
-router.get("/", root);
-app.use(render);
-app.use(router.routes());
-app.listen(3000);
+const root = async (ctx: any) => {
+  await ctx.render("index", { posts: await getPosts() });
+};
+
+const create = async ({ body, request, response, render }: any) => {
+  console.log(request, response);
+  const post = body ?? "no content";
+
+  await createPost(post);
+  await render("post", { posts: await getPosts() });
+};
+
+router.get("/", root).post("/posts", create);
+app.use(koaBody(parserOpts)).use(render).use(router.routes());
+app.listen(3001);
