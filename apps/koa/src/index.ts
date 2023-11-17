1bfc33ad859118af8a024016815bb7d5628faff0
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2023-11-17 18:37:37 +0100

    implement delete and liking of posts
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- apps/koa/src/index.ts
+++ apps/koa/src/index.ts
@@ -1,5 +1,5 @@
 import Router from "@koa/router";
-import { createPost, getPosts } from "js-app-db";
+import { createPost, deletePost, getPosts, likePost } from "js-app-db";
 import Koa from "koa";
 import type { KoaBodyMiddlewareOptions } from "koa-body";
 import { koaBody } from "koa-body";
@@ -17,6 +17,10 @@ const root = async (ctx: any) => {
   await ctx.render("index", { posts: await getPosts() });
 };
 
+const list = async ({ render }: any) => {
+  await render("posts", { posts: await getPosts() });
+};
+
 const create = async ({ body, request, response, render }: any) => {
   const post: string = request.body.post ?? "no content";
 
@@ -24,6 +28,24 @@ const create = async ({ body, request, response, render }: any) => {
   await render("posts", { posts: await getPosts() });
 };
 
-router.get("/", root).post("/posts", create);
+const remove = async ({ request, render }: any) => {
+  const id: string = request.params.id;
+  await deletePost(id);
+  await render("posts", { posts: await getPosts() });
+};
+
+const createLike = async ({ request, render }: any) => {
+  const postId: string = request.params.postId;
+  console.log(postId);
+  await likePost(postId);
+  await render("posts", { posts: await getPosts() });
+};
+
+router
+  .get("/", root)
+  .get("/posts", list)
+  .post("/posts", create)
+  .delete("/posts/:id", remove)
+  .post("/likes/:postId", createLike);
 app.use(koaBody(parserOpts)).use(render).use(router.routes());
 app.listen(3001);
